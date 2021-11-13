143780671cd605b934dd75fd6db3562c7b285316
bysrkh <bysrkh@gmail.com>
2021-11-13 13:38:07 +0700

    init commit

---
+++ Controllers/WeatherForecastController.fs
@@ -0,0 +1,13 @@
+﻿namespace user_net.Controllers
+
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+
+[<ApiController>]
+[<Route("[controller]")>]
+type WeatherForecastController (logger : ILogger<WeatherForecastController>) =
+    inherit ControllerBase()
+    
+    [<HttpGet>]
+    member _.Get() =
+        ActionResult<string>("Hello, STMIK IKMI Cirebon!")
